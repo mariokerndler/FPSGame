fc2156734a2225f453bc3022bf04353b75a8834e
Mario Kerndler <mariokerndler@gmail.com>
2020-08-04 03:27:00 +0200

    Init server and client projects

---
+++ GameServer/GameServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Server";
+
+            Server.Start(10, 26950);
+            
+            Console.ReadKey(); 
+        }
+    }
+}
